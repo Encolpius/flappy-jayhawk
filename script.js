723b0a8ca4a35b1ee6da620f927a37d5a92ed58a
Andy Hunt <andy.l.hunt85@gmail.com>
2019-02-18 15:30:04 -0500

    infinite pipes

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
-// JavaScript source code
-let pipesTop = [];
-let pipesBot = [];
+// Global variables
+let pipes = [];
+// Setup - called once when the page is loaded
 function setup() {
     createCanvas(600, 600);
-    pipe = new Pipe();
-}
-
-class Pipe {
-    constructor(x, y, height) {
-        this.x = x;
-        this.y = y;
-        this.height = height;
+    noStroke();
+    for (var i = 0; i < 5; i++) {
+        let height = Math.floor(Math.random() * 390);
+        pipes.push(new pipe(i * 200 + 560, -10, height));
     }
-
-    display() {
-        for (var i = 0; i < pipesTop.length; i++) {
-            rect(pipesTop[i].x, pipesTop[i].y, 80, pipesTop[i].height);
-            rect(pipesBot[i].x, pipesBot[i].y, 80, -pipesBot[i].height);
-            pipesTop[i].x -= 1;
-            pipesBot[i].x -= 1;
-        }
-    }
-};
-for (var i = 0; i < 25; i++) {
-    let height = Math.floor(Math.random() * 400),
-        newPipe = new Pipe(i * 200 + 560, -10, height);
-    pipesTop.push(newPipe);
-    pipesBot.push(new Pipe(i * 200 + 560, 600, 610 - (height + 100)));
 }
-
+// Continually draws objects on screen
 function draw() {
     background(173, 216, 230);
-    fill(152, 251, 152);
-    pipe.display();
-} 
+    fill(101, 67, 33);
+    rect(0, 550, width, 50);
+    stroke(25, 25, 25);
+    // Draws the ground objects
+    for (var i = 0, l = groundXs.length; i < l; i++) {
+        fill(106, 175, 106);
+        rect(groundXs[i], 540, 40, 10);
+        groundXs[i] -= 1;
+        if (groundXs[i] <= -40) {
+            groundXs[i] = width;
+        }
+    }
+    // Draws the pipes
+    for (let pipe of pipes) {
+        pipe.display();
+    }
+}
+// Creates the ground objects
+let groundXs = [];
+for (var i = 0; i < 16; i++) {
+    groundXs.push(i * 40);
+}
+// Creates the pipe objects
+
+function pipe(posX, posY, height) {
+    this.posX = posX;
+    this.posY = posY;
+    this.height = height;
+    this.display = function () {
+        rect(this.posX, this.posY, 80, this.height);
+        rect(this.posX, this.posY + (this.height + 160), 80, 540 - (this.height) - 150);
+        this.posX -= 1;
+        if (this.posX <= -200) {
+            this.posX = width + 200;
+            this.height = Math.floor(Math.random() * 390)
+        }
+    }
+}
